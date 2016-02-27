ee55f35bc715af43d5fd6831fdb9dfa324db09c3
LUISA BECERRA - CAMILO MORA <>
2016-02-27 00:46:26 -0500

    Diagrama de Flujo 3.

---
+++ src/diagramaflujo3/DiagramaFlujo3.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diagramaflujo3;
+
+/**
+ *
+ * @author Luisa Paola Becerra Pleáez--Juan Camilo Mora Tinoco
+ */
+public class DiagramaFlujo3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Primera funcion en Main");
+        func1();
+        func2();
+        System.out.println("Se termina Main"); 
+    }
+     public static int func1 (){
+        System.out.println("Segunda Funcion");
+     return 0 ;           
+    }
+     public static int func2 (){
+        System.out.println("Tercera Funcion");  
+        return 0;
+    }
+     
+}
